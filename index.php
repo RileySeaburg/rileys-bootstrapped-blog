42e967ccb524c2e9c8ff1b81f64b3dcf20e15796
Riley Seaburg <riley@rileyseaburg.com>
2020-09-05 00:25:56 -0400

    Add work in progress.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-md-8">
       <h3 class="pb-4 mb-4 font-italic border-bottom">
-        From the Firehose
+        This is a work in progress.
       </h3>
  <?php if(have_posts()) : ?>
   <?php while (have_posts()) : the_post( ) ?>
