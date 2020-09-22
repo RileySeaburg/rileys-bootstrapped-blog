035a04be989f5478feb7052091d2bd4488f4bc25
Riley Seaburg <riley@rileyseaburg.com>
2020-09-21 23:02:25 -0400

    add mautic tag

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
       <?php wp_title()?> 
     </title>
     <?php wp_head(); ?>
+    <script>
+    (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+        w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+        m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://marketing.hammersportmarketing.com/mtc.js','mt');
+
+    mt('send', 'pageview');
+</script>
   </head>
   <body>
 
